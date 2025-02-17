c6622bb935df80d6f6f7adf4a308c1e5e990ae7b
Gian Piero Canevari <canevarigian@gmail.com>
2025-02-17 19:49:09 +0100

    clase1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './platzi.webp';
 import './App.css';
+// import { link } from 'fs';
 
 function App() {
   return (
     <div className="App">
+
+      <ToDoApp />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +23,16 @@ function App() {
       </header>
     </div>
   );
+}
+
+function ToDoApp() {
+  return (
+    <li>
+      <span>V</span>
+      <p>Programar como genio</p>
+      <span>X</span>
+    </li>
+  )
 }
 
 export default App;
