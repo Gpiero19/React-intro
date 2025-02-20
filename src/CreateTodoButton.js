f9206063bc4c5029a5728d3b81ef7ed9a85b8e73
Gian Piero Canevari <canevarigian@gmail.com>
2025-02-20 17:06:14 +0100

    clase6

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,12 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton() {
     return (
-        <button class="CreateTodoButton">+</button>
+        <button class="CreateTodoButton"
+        onClick={
+            (event) => {
+                console.log('le diste click')
+                console.log(event)}}
+        >+</button>
     )
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -3,7 +3,12 @@ import './TodoSearch.css';
 function TodoSearch() {
     return (
         <input placeholder="cortar cebolla" 
-        className='TodoSearch'/>
+        className='TodoSearch'
+        onChange={(event) => {
+          console.log('Escribiste en el TODO search');
+          console.log(event);
+          console.log(event.target.value);
+        }}/>
     );
   };
 
